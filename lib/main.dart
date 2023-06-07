d79d1b9d8ee8aa22a2f884a2ad0a3d876b083b4d
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-06-07 10:38:14 +0600

    Container added

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,92 @@
+///import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp(appName: 'Hello App'),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String appName;
-
-  const MyApp({required this.appName});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: appName, home: HomeScreen());
+    return MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
       appBar: AppBar(
-        title: Text(
-          'Intro to widgets',
-          textAlign: TextAlign.start,
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w700,
-            wordSpacing: 0.5,
-            letterSpacing: 0.5,
-            fontSize: 20,
-            decoration: TextDecoration.underline,
-          ),
-        ),
-        backgroundColor: Colors.black38,
+        title: Text('Basic Widgets'),
         centerTitle: true,
       ),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text('Hello Ferdous')],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.teal,
+              height: 100,
+              width: 200,
+              margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              child: Text(
+                'Hello Ferdous',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.limeAccent,
+                border: Border.all(color: Colors.black87, width: 5),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              child: Text(
+                'Hello Ferdous',
+                style: TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Icon(
+              Icons.access_alarm,
+              color: Colors.black,
+              size: 50,
+              fill: 1,
+            ),
+            Image.network(
+              ///  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+              'https://1.bp.blogspot.com/-N9urImzUkzw/UWjpuBgRnoI/AAAAAAAA_Qc/SJNkxoaCBRc/s1600/NATOT133.JPG',
+              width: 500,
+              height: 100,
+              fit: BoxFit.contain,
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Image.asset(
+                  'assets/img.png',
+                  width: 500,
+                  height: 100,
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
