48da232b2b4a5bc79bca8da648f8702671b8a8cd
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-06-07 22:09:48 +0600

    Assignment added

--- lib/main.dart
+++ lib/main.dart
@@ -30,164 +30,24 @@ class HomeScreen extends StatelessWidget {
           onPressed: () {},
           child: Icon(Icons.mail),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.lightGreen,
-                        minimumSize: Size(50, 50),
-                        maximumSize: Size(100, 100),
-                        elevation: 15,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        shadowColor: Colors.green,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(500),
-                          side: BorderSide(color: Colors.green, width: 3),
-                        )),
-                    onPressed: () {},
-                    child: Text('Button')),
-                TextButton(
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.white24,
-                        elevation: 15,
-                        shadowColor: Colors.black,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        )),
-                    onPressed: () {},
-                    child: Text('Text Button')),
-                OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      foregroundColor: Colors.black,
-                      textStyle: TextStyle(fontSize: 15),
-                      elevation: 15,
-                      shadowColor: Colors.blue,
-                    ),
-                    onPressed: () {},
-                    child: Text('Outlineed Button')),
-                OutlinedButton.icon(
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      foregroundColor: Colors.black,
-                      textStyle: TextStyle(fontSize: 15),
-                      elevation: 15,
-                      shadowColor: Colors.blue,
-                    ),
-                    onPressed: () {},
-                    icon: Icon(Icons.account_box),
-                    label: Text('Outlineed Button with icon')),
-                IconButton(
-                    style: IconButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.black,
-                      iconSize: 5550,
-                      shadowColor: Colors.black,
-                      elevation: 15,
-                    ),
-                    onPressed: () {},
-                    icon: Icon(Icons.add_call)),
-                GestureDetector(
-                  onTap: () {
-                    print('Pressed');
-                  },
-                  onDoubleTap: () {
-                    print('Double pressed');
-                  },
-                  onLongPress: () {
-                    print('Long Pressed');
-                  },
-                  child: Image.asset(
-                    'assets/img.png',
-                    width: 100,
-                  ),
-                ),
-                InkWell(
-                    splashColor: Colors.purple,
-                    onTap: () {
-                      print('Pressed');
-                    },
-                    onDoubleTap: () {
-                      print('Double pressed');
-                    },
-                    onLongPress: () {
-                      print('Long Pressed');
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Splashing',
-                      ),
-                    )),
-                Card(
-                  elevation: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Card is added'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter your name: ',
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.teal, width: 3),
-                      ),
-                    ),
-                    textInputAction: TextInputAction.next,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your ID: ',
-                      border: OutlineInputBorder(),
-                    ),
-                    textInputAction: TextInputAction.next,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your email: ',
-                      border: OutlineInputBorder(),
-                    ),
-                    textInputAction: TextInputAction.next,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter your password: ',
-                      border: OutlineInputBorder(),
-                    ),
-                    obscureText: true,
-                    onChanged: (value) {
-                      print(value);
-                    },
-                    onSubmitted: (value) {
-                      print(value);
-                    },
-                    textAlign: TextAlign.start,
-                  ),
-                ),
-              ],
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+            color: Colors.teal,
+            height: 100,
+            width: 200,
+            margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: Text(
+              'page 1',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
             ),
           ),
-        ));
+        ])));
   }
 }
