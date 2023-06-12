40fed7f8019f3a9f5497668ac6bfbedc28538fe6
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-06-12 23:03:47 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,132 +1,123 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp(appName: 'Hello App'),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String appName;
-
-  const MyApp({required this.appName});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: appName, home: HomeScreen());
+    return MaterialApp(
+      title: 'Photo Gallery',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  List<String> students = ['Ferdous', 'Rafi', 'Talha'];
+class MyHomePage extends StatelessWidget {
+  final List<String> photoUrls = [
+    'https://images.pexels.com/photos/17133050/pexels-photo-17133050.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/17133050/pexels-photo-17133050.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/17133050/pexels-photo-17133050.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/17133050/pexels-photo-17133050.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/17133050/pexels-photo-17133050.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/17133050/pexels-photo-17133050.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+  ];
 
-  Map<int, String> universities = {
-    1: 'SUST',
-    2: 'LU',
-    3: 'MU',
-  };
+  final List<String> listItems = [
+    'Sample Photo 1',
+    'Sample Photo 2',
+    'Sample Photo 3',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(
-          'Intro to widgets',
-          textAlign: TextAlign.start,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w700,
-            wordSpacing: 0.5,
-            letterSpacing: 0.5,
-            fontSize: 20,
-            //decoration: TextDecoration.underline,
-          ),
-        ),
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.menu),
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.call)),
-        ],
-        backgroundColor: Colors.black38,
+        title: Text('Photo Gallery'),
         centerTitle: true,
       ),
-      /*body: ListView.separated(
-          itemCount: students.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(students[index]),
-            );
-          },
-          separatorBuilder: (context, index) {
-            return Divider(
-              height: 0,
-              color: Colors.red,
-            );
-          },
-        ));*/
-      /*body: ListView.separated(
-          itemCount: universities.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(universities.values.elementAt(index).toString()),
-              subtitle: Text(universities.keys.elementAt(index).toString()),
-            );
-          },
-          separatorBuilder: (context, index) {
-            return Divider(
-              height: 0,
-              color: Colors.red,
-            );
-          },
-        )*/
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.yellow,
-                alignment: Alignment.center,
-              ),
-              Positioned(
-                bottom: 20,
-                right: 20,
-                left: 20,
-                top: 20,
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  color: Colors.redAccent,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.all(16.0),
+              child: Text(
+                'Welcome to My Photo Gallery!',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Positioned(
-                  bottom: 40,
-                  right: 40,
-                  left: 40,
-                  top: 40,
-                  child: Container(
-                    width: 25,
-                    height: 25,
-                    color: Colors.black,
-                  )),
-              Positioned.fill(
-                  child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  'Hello Ferdous',
-                  style: TextStyle(
-                    color: Colors.white,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.search),
+                  hintText: 'Search',
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.all(10.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Wrap(
+              spacing: 8.0,
+              runSpacing: 8.0,
+              alignment: WrapAlignment.center,
+              children: photoUrls.map((url) {
+                return ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Clicked on photo!')),
+                    );
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.network(
+                        url,
+                        width: 100.0,
+                        height: 100.0,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(height: 6.0),
+                      Text('Caption'),
+                    ],
                   ),
-                ),
-              ))
-            ],
-          )
-        ],
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 10.0),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: listItems.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(photoUrls[index]),
+                  ),
+                  title: Text(listItems[index]),
+                  subtitle: Text('Category'),
+                );
+              },
+            ),
+            SizedBox(height: 16.0),
+            IconButton(
+              icon: Icon(Icons.upload),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Photos Uploaded Successfully!')),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
