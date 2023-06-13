1fad59de284e4d8767594a113a0bcffef87c479c
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-06-13 12:54:51 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -22,119 +22,120 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Basic Widgets'),
-          centerTitle: true,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.mail),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.lightGreen,
-                        minimumSize: Size(50, 50),
-                        maximumSize: Size(100, 100),
-                        elevation: 15,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        shadowColor: Colors.green,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(500),
-                          side: BorderSide(color: Colors.green, width: 3),
-                        )),
-                    onPressed: () {},
-                    child: Text('Button')),
-                TextButton(
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.white24,
-                        elevation: 15,
-                        shadowColor: Colors.black,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        )),
-                    onPressed: () {},
-                    child: Text('Text Button')),
-                OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      foregroundColor: Colors.black,
-                      textStyle: TextStyle(fontSize: 15),
+      appBar: AppBar(
+        title: Text('Basic Widgets'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.mail),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightGreen,
+                      minimumSize: Size(50, 50),
+                      maximumSize: Size(100, 100),
                       elevation: 15,
-                      shadowColor: Colors.blue,
-                    ),
-                    onPressed: () {},
-                    child: Text('Outlineed Button')),
-                OutlinedButton.icon(
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      foregroundColor: Colors.black,
-                      textStyle: TextStyle(fontSize: 15),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      shadowColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(500),
+                        side: BorderSide(color: Colors.green, width: 3),
+                      )),
+                  onPressed: () {},
+                  child: Text('Button')),
+              TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white24,
                       elevation: 15,
-                      shadowColor: Colors.blue,
-                    ),
-                    onPressed: () {},
-                    icon: Icon(Icons.account_box),
-                    label: Text('Outlineed Button with icon')),
-                IconButton(
-                    style: IconButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.black,
-                      iconSize: 5550,
                       shadowColor: Colors.black,
-
-                      elevation: 15,
-                    ),
-                    onPressed: () {},
-                    icon: Icon(Icons.add_call)),
-                GestureDetector(
-                  onTap: () {
-                    print('Pressed');
-                  },
-                  onDoubleTap: () {
-                    print('Double pressed');
-                  },
-                  onLongPress: () {
-                    print('Long Pressed');
-                  },
-                  child: Image.asset(
-                    'assets/img.png',
-                    width: 100,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      )),
+                  onPressed: () {},
+                  child: Text('Text Button')),
+              OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    foregroundColor: Colors.black,
+                    textStyle: TextStyle(fontSize: 15),
+                    elevation: 15,
+                    shadowColor: Colors.blue,
+                  ),
+                  onPressed: () {},
+                  child: Text('Outlineed Button')),
+              OutlinedButton.icon(
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    foregroundColor: Colors.black,
+                    textStyle: TextStyle(fontSize: 15),
+                    elevation: 15,
+                    shadowColor: Colors.blue,
+                  ),
+                  onPressed: () {},
+                  icon: Icon(Icons.account_box),
+                  label: Text('Outlineed Button with icon')),
+              IconButton(
+                  style: IconButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.black,
+                    iconSize: 5550,
+                    shadowColor: Colors.black,
+                    elevation: 15,
+                  ),
+                  onPressed: () {},
+                  icon: Icon(Icons.add_call)),
+              GestureDetector(
+                onTap: () {
+                  print('Pressed');
+                },
+                onDoubleTap: () {
+                  print('Double pressed');
+                },
+                onLongPress: () {
+                  print('Long Pressed');
+                },
+                child: Image.asset(
+                  'assets/img.png',
+                  width: 100,
+                ),
+              ),
+              InkWell(
+                splashColor: Colors.purple,
+                onTap: () {
+                  print('Pressed');
+                },
+                onDoubleTap: () {
+                  print('Double pressed');
+                },
+                onLongPress: () {
+                  print('Long Pressed');
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Splashing',
                   ),
                 ),
-                InkWell(
-                    splashColor: Colors.purple,
-                    onTap: () {
-                      print('Pressed');
-                    },
-                    onDoubleTap: () {
-                      print('Double pressed');
-                    },
-                    onLongPress: () {
-                      print('Long Pressed');
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Splashing',
-                      ),
-                    )),
-                Card(
-                  elevation: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Card is added'),
-                  ),
+              ),
+              Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Card is added'),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
