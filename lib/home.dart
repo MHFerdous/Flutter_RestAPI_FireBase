2a40cad8eff613847a00f9c847c9bfe11e26ca13
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-06-17 00:44:39 +0600

    Navigator added

---
+++ lib/home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_application/products_list.dart';
+import 'package:mobile_application/users_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Home Screen'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ProductListScreen(),
+                  ),
+                );
+              },
+              child: const Text('Go to product List'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const UserScreen(),
+                  ),
+                );
+              },
+              child: const Text('Go to User List'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_application/counter_screen.dart';
+import 'home.dart';
 
-void main() {
-  runApp(
-    const MyApp(
-      appName: 'Hello App',
-    ),
-  );
+main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String appName;
-  const MyApp({required this.appName});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appName,
-      home: const CounterScreen(),
-    );
-  }
-}
-
-/*
-class GoodScreen extends StatefulWidget {
-  const GoodScreen({Key? key}) : super(key: key);
-
-  @override
-  State<GoodScreen> createState() => _GoodScreenState();
-}
-
-class _GoodScreenState extends State<GoodScreen> {
-  String welcomeMessage = 'HI';
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-  }
-
-  @override
-  void didUpdateWidget(covariant GoodScreen oldWidget) {
-    super.didUpdateWidget(oldWidget);
-  }
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My App'),
-      ),
-      body: Center(
-        child: Text(
-          welcomeMessage,
-          style: TextStyle(fontSize: 25),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          welcomeMessage = welcomeMessage == 'HI' ? 'HELLO' : 'HI';
-          print(welcomeMessage);
-          setState(() {});
-        },
-        label: Text('press me'),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
-
-  @override
-  void deactivate() {
-    super.deactivate();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
 }
-*/
