9ba6b644347297fc2d027e40dfd3c2cb493bfe25
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-06-20 15:50:21 +0600

    Updated

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_application/product_screen..dart';
 
 class CartPage extends StatelessWidget {
   final List<Product> products;
 
-  CartPage({required this.products});
+  const CartPage({super.key, required this.products});
 
   int getTotalQuantity() {
     int total = 0;
