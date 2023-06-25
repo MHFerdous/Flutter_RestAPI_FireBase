9aace710d18d2c4861b2810cdbe65e224e5790c7
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-06-25 12:20:07 +0600

    Updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,14 +8,89 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<task> tasks = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: const Text('Task Management'),
         centerTitle: true,
       ),
+      body: ListView.separated(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onLongPress: () {},
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const Divider();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Add Task'),
+                content: const Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        label: Text('title:'),
+                        focusedBorder: OutlineInputBorder(),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 4,
+                      decoration: InputDecoration(
+                        label: Text('Description:'),
+                        focusedBorder: OutlineInputBorder(),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        label: Text('Days required:'),
+                        focusedBorder: OutlineInputBorder(),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('save'),
+                  )
+                ],
+                contentPadding: const EdgeInsets.all(25),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
+}
+
+class task {
+  String title, description;
+  int date;
+  task(this.title, this.description, this.date);
 }
