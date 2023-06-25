c6342bedaa2ddad129caa0f8165b23a77e5102c3
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-06-25 11:17:50 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,18 @@
 import 'package:flutter/material.dart';
+
 import 'home_screen.dart';
 
-main() {
+main(){
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-bool iconBool = false;
-IconData iconLight = Icons.wb_sunny;
-IconData iconDark = Icons.nights_stay;
-
-ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  primarySwatch: Colors.red,
-);
-
-ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  primarySwatch: Colors.red,
-);
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TODO',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey.shade700,
-          title: const Text(
-            'TODO App',
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          ),
-          centerTitle: true,
-          leading: IconButton(
-            onPressed: () {
-              setState(() {
-                iconBool = !iconBool;
-              });
-            },
-            icon: const Icon(Icons.dark_mode),
-          ),
-        ),
-      ),
-      theme: iconBool ? darkTheme : lightTheme,
-      themeMode: ThemeMode.dark,
-      /*theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.red,
-          floatingActionButtonTheme: const FloatingActionButtonThemeData(
-            backgroundColor: Colors.blueGrey,
-          )),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.red,
-        // it will not work now as the color of the buttons are already applied from button widgets
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Colors.green,
-        ),
-      ),*/
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
