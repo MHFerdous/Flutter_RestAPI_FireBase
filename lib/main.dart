7b69330234b6ba12e0465a5c7d3c4cf9cf6c345c
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-02 15:06:23 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_notifications/home_screen.dart';
 
-main() {
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message)async {
+  await Firebase.initializeApp();
+}
+
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            ListTile(
-              title: const Text('Title:'),
-              subtitle: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Price:'),
-                  SizedBox(height: 100, child: Text('Description: ')),
-                ],
-              ),
-              trailing: Image.network(
-                  'https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=400'),
-            ),
-            ListTile(
-              title: const Text('Title:'),
-              subtitle: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Price:'),
-                  SizedBox(height: 100, child: Text('Description: ')),
-                ],
-              ),
-              trailing: Image.network(
-                  'https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=400'),
-            ),
-            ListTile(
-              title: const Text('Title:'),
-              subtitle: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Price:'),
-                  SizedBox(height: 100, child: Text('Description: ')),
-                ],
-              ),
-              trailing: Image.network(
-                  'https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=400'),
-            ),
-          ],
-        ),
+        primarySwatch: Colors.blue,
       ),
+      home:  HomeScreen(),
     );
   }
 }
+
