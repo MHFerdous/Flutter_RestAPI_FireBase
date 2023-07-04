7dedf7e2e24f0be53661d0fe355df04ab54003ad
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-04 22:12:10 +0600

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -8,11 +8,75 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Map<String, List<String>> contacts = {
+    'Ferdous': ['abc@gmail.com', '0123456'],
+    'Talha': ['def@gmail.com', '0123789'],
+    'Hasan': ['jhi@gmail.com', '0127865']
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: const Text('Contact List'),
+        centerTitle: true,
+      ),
+      body: ListView.separated(
+        itemCount: contacts.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => const Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Contact Details',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Name: Ferdous',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Email: abc@gmail.com',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Phone Number: 01234567989',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    )
+                  ],
+                ),
+              );
+            },
+            title: Text('$contacts'),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
       ),
     );
   }
