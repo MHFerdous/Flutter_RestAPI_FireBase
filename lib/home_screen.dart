51778c3a23cea17c538b83e066dfee542f7355cb
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-07 19:41:28 +0600

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -14,6 +14,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<Product> products = [];
 
+  bool inProgress = false;
+
   @override
   void initState() {
     super.initState();
@@ -21,6 +23,8 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void getProducts() async {
+    inProgress = true;
+    setState(() {});
     Response response = await get(
       Uri.parse('https://crud.teamrabbil.com/api/v1/ReadProduct'),
     );
@@ -30,16 +34,14 @@ class _HomeScreenState extends State<HomeScreen> {
     //print(decodedResponse['data'].length);
 
     if (response.statusCode == 200 && decodedResponse['status'] == 'success') {
-      decodedResponse['data'].forEach(
-        (e) {
-          products.add(
-            Product(e['_id'], e['ProductName'], e['ProductCode'], e['Img'],
-                e['UnitPrice'], e['TotalPrice'], e['CreatedDate'], e['Qty']),
-          );
-        },
-      );
-      setState(() {});
+      for (var e in decodedResponse['data']) {
+        products.add(
+          Product.toJson(e),
+        );
+      }
     }
+    inProgress = false;
+    setState(() {});
   }
 
   @override
@@ -60,79 +62,83 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         child: const Icon(Icons.add),
       ),
-      body: ListView.separated(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            onLongPress: () {
-              showDialog(
-                context: context,
-                builder: (_) {
-                  return AlertDialog(
-                    titlePadding: const EdgeInsets.only(left: 16, top: 8),
-                    contentPadding:
-                        const EdgeInsets.only(left: 8, right: 8, bottom: 8),
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text('Choose an action'),
-                        IconButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: const Icon(Icons.close),
-                        ),
-                      ],
-                    ),
-                    content: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ListTile(
-                          onTap: () {},
-                          leading: const Icon(Icons.edit),
-                          title: const Text('Edit'),
-                        ),
-                        const Divider(
-                          height: 0,
-                        ),
-                        ListTile(
-                          onTap: () {},
-                          leading: const Icon(Icons.delete),
-                          title: const Text('Delete'),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              );
-            },
-            title: Text('Product Name: ${products[index].productName}'),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Product Code: ${products[index].productCode}'),
-                Text('Total Price: ${products[index].totalPrice}'),
-                Text('Quantity: ${products[index].quantity}'),
-              ],
-            ),
-            leading: Image.network(
-              products[index].image,
-              width: 50,
-              height: 50,
-              errorBuilder: (_, __, ___) {
-                return const Icon(
-                  Icons.image,
-                  size: 32,
+      body: inProgress
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.separated(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  onLongPress: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) {
+                        return AlertDialog(
+                          titlePadding: const EdgeInsets.only(left: 16, top: 8),
+                          contentPadding: const EdgeInsets.only(
+                              left: 8, right: 8, bottom: 8),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Choose an action'),
+                              IconButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                icon: const Icon(Icons.close),
+                              ),
+                            ],
+                          ),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              ListTile(
+                                onTap: () {},
+                                leading: const Icon(Icons.edit),
+                                title: const Text('Edit'),
+                              ),
+                              const Divider(
+                                height: 0,
+                              ),
+                              ListTile(
+                                onTap: () {},
+                                leading: const Icon(Icons.delete),
+                                title: const Text('Delete'),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  title: Text('Product Name: ${products[index].productName}'),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Product Code: ${products[index].productCode}'),
+                      Text('Total Price: ${products[index].totalPrice}'),
+                      Text('Quantity: ${products[index].quantity}'),
+                    ],
+                  ),
+                  leading: Image.network(
+                    products[index].image,
+                    width: 50,
+                    height: 50,
+                    errorBuilder: (_, __, ___) {
+                      return const Icon(
+                        Icons.image,
+                        size: 32,
+                      );
+                    },
+                  ),
+                  trailing: Text('Unit Price: \n${products[index].unitPrice}'),
                 );
               },
+              separatorBuilder: (BuildContext context, int index) {
+                return const Divider();
+              },
             ),
-            trailing: Text('Unit Price: \n${products[index].unitPrice}'),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const Divider();
-        },
-      ),
     );
   }
 }
--- lib/product.dart
+++ lib/product.dart
@@ -10,4 +10,17 @@ class Product {
 
   Product(this.id, this.productName, this.productCode, this.image,
       this.unitPrice, this.totalPrice, this.createdAt, this.quantity);
+
+  factory Product.toJson(Map<String, dynamic> e) {
+    return Product(
+      e['_id'],
+      e['ProductName'],
+      e['ProductCode'],
+      e['Img'],
+      e['UnitPrice'],
+      e['TotalPrice'],
+      e['CreatedDate'],
+      e['Qty'],
+    );
+  }
 }
