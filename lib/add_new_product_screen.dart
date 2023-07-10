05b3cd6cd46741c70699d6b659d0143be6752323
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-10 16:00:07 +0600

    updated

--- lib/add_new_product_screen.dart
+++ lib/add_new_product_screen.dart
@@ -8,6 +8,16 @@ class AddNewProductScreen extends StatefulWidget {
 }
 
 class _AddNewProductScreenState extends State<AddNewProductScreen> {
+
+  final TextEditingController _nameTEController = TextEditingController();
+  final TextEditingController _productCodeTEController = TextEditingController();
+  final TextEditingController _priceTEController = TextEditingController();
+  final TextEditingController _quantityTEController = TextEditingController();
+  final TextEditingController _totalPriceTEController = TextEditingController();
+  final TextEditingController _imageTEController = TextEditingController();
+
+  final GlobalKey<FormState> formState = GlobalKey<formState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,64 +28,80 @@ class _AddNewProductScreenState extends State<AddNewProductScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Product Name',
+          child: Form(
+            key: _formstate,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nameTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Product Name',
+                  ),
+                  validator: (String?value){
+
+                  },
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Product Code',
+                const SizedBox(
+                  height: 16,
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Product Price',
+                TextFormField(
+                  controller: _productCodeTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Product Code',
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Quantity',
+                const SizedBox(
+                  height: 16,
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Total Price',
+                TextFormField(
+                  controller: _priceTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Product Price',
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Image',
+                const SizedBox(
+                  height: 16,
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Add'),
+                TextFormField(
+                  controller: _quantityTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Quantity',
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: _totalPriceTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Total Price',
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: _imageTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Image',
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if(_fromstate.currentState!.validate()){
+
+                      }
+                    },
+                    child: const Text('Add'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
