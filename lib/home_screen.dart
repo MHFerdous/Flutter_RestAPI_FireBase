347a86d9a72624411d00b3c222fd1714b95e01b9
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-10 23:41:42 +0600

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,40 +12,79 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Product> products = [];
-
-  bool inProgress = false;
+  var recipes = [
+    {
+      "title": "Pasta Carbonara",
+      "description": "Creamy pasta dish with bacon and cheese.",
+      "ingredients": ["spaghetti", "bacon", "egg", "cheese"]
+    },
+    {
+      "title": "Caprese Salad",
+      "description":
+          "Simple and refreshing salad with tomatoes, mozzarella, and basil.",
+      "ingredients": ["tomatoes", "mozzarella", "basil"]
+    },
+    {
+      "title": "Banana Smoothie",
+      "description": "Healthy and creamy smoothie with bananas and milk.",
+      "ingredients": ["bananas", "milk"]
+    },
+    {
+      "title": "Chicken Stir-Fry",
+      "description": "Quick and flavorful stir-fried chicken with vegetables.",
+      "ingredients": ["chicken breast", "broccoli", "carrot", "soy sauce"]
+    },
+    {
+      "title": "Grilled Salmon",
+      "description": "Delicious grilled salmon with lemon and herbs.",
+      "ingredients": ["salmon fillet", "lemon", "olive oil", "dill"]
+    },
+    {
+      "title": "Vegetable Curry",
+      "description": "Spicy and aromatic vegetable curry.",
+      "ingredients": ["mixed vegetables", "coconut milk", "curry powder"]
+    },
+    {
+      "title": "Berry Parfait",
+      "description": "Layered dessert with fresh berries and yogurt.",
+      "ingredients": ["berries", "yogurt", "granola"]
+    },
+  ];
 
   @override
   void initState() {
     super.initState();
-    getProducts();
-  }
-
-  void getProducts() async {
-    inProgress = true;
-    setState(() {});
-    Response response = await get(
-      Uri.parse('https://crud.teamrabbil.com/api/v1/ReadProduct'),
-    );
-    print(response.statusCode);
-    print(response.body);
-    final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
-    //print(decodedResponse['data'].length);
-
-    if (response.statusCode == 200 && decodedResponse['status'] == 'success') {
-      for (var e in decodedResponse['data']) {
-        products.add(
-          Product.toJson(e),
-        );
-      }
-    }
-    inProgress = false;
-    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Food Recipe'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Scrollbar(
+            thickness: 10,
+            child: ListView.separated(
+              itemCount: recipes.length,
+              primary: false,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(recipes[index]['title']!),
+                  subtitle: Text(recipes[index]['description']!),
+                  leading: Icon(Icons.fastfood),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const Divider();
+              },
+            ),
+          ),
+        ]
+        ),
+      ),
+    );
   }
 }
