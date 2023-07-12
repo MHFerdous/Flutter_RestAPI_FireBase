dcb049b0bf3b2a610228f913c5ba391cb35780dc
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-12 22:13:51 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,93 +24,94 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Basic Widgets'),
-          centerTitle: true,
-        ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      label: Text('Enter your name: '),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red),
+      appBar: AppBar(
+        title: Text('Basic Widgets'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    label: Text('Enter your name: '),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.teal, width: 3),
+                    ),
+                  ),
+                  textInputAction: TextInputAction.next,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextField(
+                  controller: _IDEditingController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    label: Text('Enter your ID: '),
+                    border: OutlineInputBorder(),
+                  ),
+                  textInputAction: TextInputAction.next,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    label: Text('Enter your email: '),
+                    border: OutlineInputBorder(),
+                  ),
+                  textInputAction: TextInputAction.next,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextField(
+                  controller: _passwordTextEditingController,
+                  decoration: InputDecoration(
+                      label: Text(
+                        'Password: ',
+                        style: TextStyle(
+                          color: Colors.red,
+                        ),
                       ),
+                      hintText: 'Enter your password: ',
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.teal, width: 3),
                       ),
-                    ),
-                    textInputAction: TextInputAction.next,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: TextField(
-                    controller: _IDEditingController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      label: Text('Enter your ID: '),
-                      border: OutlineInputBorder(),
-                    ),
-                    textInputAction: TextInputAction.next,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      label: Text('Enter your email: '),
-                      border: OutlineInputBorder(),
-                    ),
-                    textInputAction: TextInputAction.next,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: TextField(
-                    controller: _passwordTextEditingController,
-                    decoration: InputDecoration(
-                        label: Text(
-                          'Password: ',
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                        hintText: 'Enter your password: ',
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.teal, width: 3),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green),
-                        )),
-                    obscureText: true,
-                    onChanged: (value) {
-                      print(value);
-                    },
-                    onSubmitted: (value) {
-                      print(value);
-                    },
-                    textAlign: TextAlign.start,
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print(_IDEditingController.text);
-                    print(_passwordTextEditingController.text);
-                    _passwordTextEditingController.text = 'hgh';
-                    _passwordTextEditingController.clear();
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                      )),
+                  obscureText: true,
+                  onChanged: (value) {
+                    print(value);
                   },
-                  child: Text('Done'),
-                )
-              ],
-            ),
+                  onSubmitted: (value) {
+                    print(value);
+                  },
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_IDEditingController.text);
+                  print(_passwordTextEditingController.text);
+                  _passwordTextEditingController.text = 'hgh';
+                  _passwordTextEditingController.clear();
+                },
+                child: Text('Done'),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
