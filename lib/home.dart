fe25ea5a4502abce3fa30d057fe2cfbbbbc49c77
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-16 11:39:55 +0600

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 
-
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
