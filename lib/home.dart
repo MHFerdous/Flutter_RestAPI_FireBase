eb9a33d432d4bd74321cc9bc9a86ab4bba98f644
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-16 11:51:25 +0600

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,15 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Profile'),
       ),
+
+      body: Column(
+        children: [
+          Placeholder(
+            child: Image.network('https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/lioness-displaying-dangerous-teeth-in-a-rainstorm-johan-swanepoel.jpg'),
+          )
+        ],
+      ),
+
     );
   }
 }
