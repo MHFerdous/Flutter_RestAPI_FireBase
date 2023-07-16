7a10cf6227a086392a9abb8496211fd9b6c3ce3a
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-16 11:14:52 +0600

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_application/products_list.dart';
-import 'package:mobile_application/users_screen.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,38 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home (1)'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Home Screen'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ProductListScreen(),
-                  ),
-                );
-              },
-              child: const Text('Go to product List'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const UserScreen(),
-                  ),
-                );
-              },
-              child: const Text('Go to User List'),
-            ),
-          ],
-        ),
+        title: const Text('Profile'),
       ),
     );
   }
