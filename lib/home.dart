e4d10a64b8f9acf62fdbf0f78e9d5ef10f91a772
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-20 20:51:11 +0600

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,81 +1,195 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  Map<String, List<String>> contacts = {
-    'Ferdous': ['abc@gmail.com', '0123456'],
-    'Talha': ['def@gmail.com', '0123789'],
-    'Hasan': ['jhi@gmail.com', '0127865']
-  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Contact List'),
+        title: const Text('Beautiful Hasan'),
         centerTitle: true,
       ),
-      body: ListView.separated(
-        itemCount: contacts.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            onTap: () {
-              showModalBottomSheet(
-                context: context,
-                builder: (context) => const Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Contact Details',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.portrait) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      radius: 180,
+                      backgroundImage: NetworkImage(
+                          'https://media.licdn.com/dms/image/D5603AQEeifD0eSgOZA/profile-displayphoto-shrink_800_800/0/1688810978987?e=1695254400&v=beta&t=-5TlTN7rzAIT-YVa8cnWGMHga4J6ZNt45mOpEbQtgbE'),
+                    ),
+                  ),
+                  const ListTile(
+                    title: Padding(
+                      padding: EdgeInsets.only(top: 5, bottom: 10),
+                      child: Center(
+                        child: Text(
+                          'Hasan',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Name: Ferdous',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                    subtitle: Text(
+                      'The boy who loves flirting and dirty language. He flirts with everyone, specially with Kamran',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Email: abc@gmail.com',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                /*  Wrap(
+                    spacing: 10,
+                    children: [
+                      Image.network(
+                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                        height: 90,
+                        width: 110,
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Phone Number: 01234567989',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                      Image.network(
+                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                        height: 90,
+                        width: 110,
                       ),
-                    ),
-                    SizedBox(
-                      height: 50,
-                    )
-                  ],
+                      Image.network(
+                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                        height: 90,
+                        width: 110,
+                      ),
+                      Image.network(
+                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                        height: 90,
+                        width: 110,
+                      ),
+                      Image.network(
+                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                        height: 90,
+                        width: 110,
+                      ),
+                      Image.network(
+                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                        height: 90,
+                        width: 110,
+                      ),
+                      Image.network(
+                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                        height: 90,
+                        width: 110,
+                      ),
+                      Image.network(
+                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                        height: 90,
+                        width: 110,
+                      ),
+                      Image.network(
+                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                        height: 90,
+                        width: 110,
+                      ),
+                    ],
+                  )*/
+                ],
+              ),
+            );
+          }
+
+          return Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius: 180,
+                  backgroundImage: NetworkImage(
+                      'https://images.pexels.com/photos/1761279/pexels-photo-1761279.jpeg?auto=compress&cs=tinysrgb&w=600'),
                 ),
-              );
-            },
-            title: Text('$contacts'),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const ListTile(
+                        title: Padding(
+                          padding: EdgeInsets.only(top: 5, bottom: 10),
+                          child: Text(
+                            'Nature',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Study nature, love nature, stay close to nature. It will never fail you. —Frank Lloyd Wright',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Wrap(
+                        spacing: 10,
+                        children: [
+                          Image.network(
+                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                            height: 90,
+                            width: 110,
+                          ),
+                          Image.network(
+                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                            height: 90,
+                            width: 110,
+                          ),
+                          Image.network(
+                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                            height: 90,
+                            width: 110,
+                          ),
+                          Image.network(
+                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                            height: 90,
+                            width: 110,
+                          ),
+                          Image.network(
+                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                            height: 90,
+                            width: 110,
+                          ),
+                          Image.network(
+                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                            height: 90,
+                            width: 110,
+                          ),
+                          Image.network(
+                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                            height: 90,
+                            width: 110,
+                          ),
+                          Image.network(
+                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                            height: 90,
+                            width: 110,
+                          ),
+                          Image.network(
+                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
+                            height: 90,
+                            width: 110,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const Divider();
         },
       ),
     );
