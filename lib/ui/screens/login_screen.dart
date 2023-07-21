e4d70c2bc8cdfc432e78ad88630aeccb2e218eb4
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-21 22:38:53 +0600

    updated

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_application/ui/widgets/screen_background.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ScreenBackground(
+        child: Column(
+
+        ),
+
+      ),
+    );
   }
 }
--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mobile_application/ui/screens/login_screen.dart';
 import 'package:mobile_application/ui/utils/assets_utils.dart';
+import 'package:mobile_application/ui/widgets/screen_background.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -35,24 +36,14 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: SvgPicture.asset(
-              AssetsUtils.backgroundSVG,
-              fit: BoxFit.fitHeight,
-            ),
+      body: ScreenBackground(
+        child: Center(
+          child: SvgPicture.asset(
+            AssetsUtils.logoSVG,
+            width: 90,
+            fit: BoxFit.scaleDown,
           ),
-          Center(
-            child: SvgPicture.asset(
-              AssetsUtils.logoSVG,
-              width: 90,
-              fit: BoxFit.scaleDown,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
