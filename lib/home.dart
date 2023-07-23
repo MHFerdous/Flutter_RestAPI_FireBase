36e7deb78211978d8dff80ffb1eadc5490d66410
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-23 21:40:37 +0600

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,196 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_application/second_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _nameTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+
+  GlobalKey<FormState> regForm = GlobalKey<FormState>();
+
+  bool _isObscure = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Beautiful Hasan'),
-        centerTitle: true,
+        title: const Text('Registration Form'),
       ),
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          if (orientation == Orientation.portrait) {
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: CircleAvatar(
-                      radius: 180,
-                      backgroundImage: NetworkImage(
-                          'https://media.licdn.com/dms/image/D5603AQEeifD0eSgOZA/profile-displayphoto-shrink_800_800/0/1688810978987?e=1695254400&v=beta&t=-5TlTN7rzAIT-YVa8cnWGMHga4J6ZNt45mOpEbQtgbE'),
-                    ),
-                  ),
-                  const ListTile(
-                    title: Padding(
-                      padding: EdgeInsets.only(top: 5, bottom: 10),
-                      child: Center(
-                        child: Text(
-                          'Hasan',
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    subtitle: Text(
-                      'The boy who loves flirting and dirty language. He flirts with everyone, specially with Kamran',
+      body: Center(
+        child: Form(
+          key: regForm,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  controller: _nameTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Name',
+                    label: Text(
+                      'Input your name',
                       style: TextStyle(
                         fontSize: 18,
-                        fontWeight: FontWeight.bold,
                       ),
                     ),
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(),
                   ),
-                /*  Wrap(
-                    spacing: 10,
-                    children: [
-                      Image.network(
-                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                        height: 90,
-                        width: 110,
-                      ),
-                      Image.network(
-                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                        height: 90,
-                        width: 110,
-                      ),
-                      Image.network(
-                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                        height: 90,
-                        width: 110,
-                      ),
-                      Image.network(
-                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                        height: 90,
-                        width: 110,
-                      ),
-                      Image.network(
-                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                        height: 90,
-                        width: 110,
-                      ),
-                      Image.network(
-                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                        height: 90,
-                        width: 110,
-                      ),
-                      Image.network(
-                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                        height: 90,
-                        width: 110,
-                      ),
-                      Image.network(
-                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                        height: 90,
-                        width: 110,
-                      ),
-                      Image.network(
-                        'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                        height: 90,
-                        width: 110,
-                      ),
-                    ],
-                  )*/
-                ],
-              ),
-            );
-          }
-
-          return Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: CircleAvatar(
-                  radius: 180,
-                  backgroundImage: NetworkImage(
-                      'https://images.pexels.com/photos/1761279/pexels-photo-1761279.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                  textInputAction: TextInputAction.next,
+                  validator: (String? value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
                 ),
               ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      const ListTile(
-                        title: Padding(
-                          padding: EdgeInsets.only(top: 5, bottom: 10),
-                          child: Text(
-                            'Nature',
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        subtitle: Text(
-                          'Study nature, love nature, stay close to nature. It will never fail you. —Frank Lloyd Wright',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  controller: _passwordTEController,
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    label: const Text(
+                      'Input your password',
+                      style: TextStyle(
+                        fontSize: 18,
                       ),
-                      Wrap(
-                        spacing: 10,
-                        children: [
-                          Image.network(
-                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                            height: 90,
-                            width: 110,
-                          ),
-                          Image.network(
-                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                            height: 90,
-                            width: 110,
-                          ),
-                          Image.network(
-                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                            height: 90,
-                            width: 110,
-                          ),
-                          Image.network(
-                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                            height: 90,
-                            width: 110,
-                          ),
-                          Image.network(
-                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                            height: 90,
-                            width: 110,
-                          ),
-                          Image.network(
-                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                            height: 90,
-                            width: 110,
-                          ),
-                          Image.network(
-                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                            height: 90,
-                            width: 110,
-                          ),
-                          Image.network(
-                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                            height: 90,
-                            width: 110,
-                          ),
-                          Image.network(
-                            'https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/charleston-sc-edisto-island-botany-bay-road-dave-allen.jpg',
-                            height: 90,
-                            width: 110,
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    border: const OutlineInputBorder(),
+                    focusedBorder: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      },
+                      icon: Icon(
+                          _isObscure ? Icons.visibility : Icons.visibility_off),
+                    ),
                   ),
+                  validator: (String? value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Please enter your password';
+                    }
+                    if (value!.length < 6) {
+                      return 'Its too short';
+                    }
+                    return null;
+                  },
                 ),
               ),
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (regForm.currentState!.validate()) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SecondScreen(),
+                          ),
+                        );
+                        _nameTEController.clear();
+                        _passwordTEController.clear();
+                      }
+                    },
+                    child: const Text('Sign in'),
+                  ),
+                ),
+              )
             ],
-          );
-        },
+          ),
+        ),
       ),
     );
   }
