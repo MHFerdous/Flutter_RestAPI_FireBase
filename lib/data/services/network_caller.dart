855441e3092e6d3eb0d87c4d7b456395d952f71d
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-07-26 20:10:24 +0600

    updated

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,52 @@
+import 'dart:developer';
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:mobile_application/data/models/network_response.dart';
+
+class NetworkCaller {
+  Future<NetworkResponse> getRequest(String url) async {
+    try {
+      Response response = await get(
+        Uri.parse(url),
+      );
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+          true,
+          response.statusCode,
+          jsonDecode(response.body),
+        );
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(
+        e.toString(),
+      );
+    }
+    return NetworkResponse(false, -1, null);
+  }
+
+  Future<NetworkResponse> postRequest(
+      String url, Map<String, dynamic> body) async {
+    try {
+      Response response = await post(
+        Uri.parse(url),
+        body: jsonEncode(body),
+      );
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+          true,
+          response.statusCode,
+          jsonDecode(response.body),
+        );
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(
+        e.toString(),
+      );
+    }
+    return NetworkResponse(false, -1, null);
+  }
+}
