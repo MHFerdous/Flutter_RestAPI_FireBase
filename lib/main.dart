e70b20974cf755e24fa9b57fae812e25f93a1aa0
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-08-18 12:03:27 +0600

    Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -7,17 +8,99 @@ Future<void> main() async {
   await Firebase.initializeApp();
 
   runApp(
-    const CartApp(),
+    const BasketBallLiveScoreApp(),
   );
 }
 
-class CartApp extends StatelessWidget {
-  const CartApp({Key? key}) : super(key: key);
+class BasketBallLiveScoreApp extends StatelessWidget {
+  const BasketBallLiveScoreApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase',
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+
+
+  Future<void> getDataFromFirebase() async {
+    CollectionReference basketBallRef = firebaseFirestore.collection('basketball');
+    final DocumentReference documentReference = basketBallRef.doc('2023_ban_vs_ind');
+    final data = await documentReference.get();
+    print(data.data());
+
+  }
+  @override
+  void initState(){
+    getDataFromFirebase();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BasketBall Live'),
+      ),
+      body: Center(
+        child: Column(
+
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Text(
+              'Match Name',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      '12',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Text(
+                      'Team name',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ],
+                ),
+                const Text('vs'),
+                Column(
+                  children: [
+                    Text(
+                      '12',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Text(
+                      'Team name',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
