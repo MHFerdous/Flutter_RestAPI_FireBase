c66bdfd02924ba63915db67217fb8808ef3f1a0f
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-08-20 20:43:08 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -23,30 +24,8 @@ class BasketBallLiveScoreApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-
-  Future<void> getDataFromFirebase() async {
-    CollectionReference basketBallRef =
-        firebaseFirestore.collection('basketball');
-    final DocumentReference documentReference =
-        basketBallRef.doc('2023_ban_vs_ind');
-    final data = await documentReference.get();
-    print(data.data());
-  }
-
-  @override
-  void initState() {
-    getDataFromFirebase();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,77 +33,71 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('BasketBall Live'),
       ),
-      body: FutureBuilder(
-        future: firebaseFirestore
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
             .collection('basketball')
             .doc('2023_ban_vs_ind')
-            .get(),
+            .snapshots(),
         builder: (context, AsyncSnapshot<DocumentSnapshot<Object?>> snapshot) {
-          print(snapshot.data?.data());
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(snapshot.error.toString()),
-              );
-            }
-            if (snapshot.hasData) {
-              final score = snapshot.data!;
-              return Center(
-                child: Column(
+          log(snapshot.data!.data().toString());
+
+          final score = snapshot.data!;
+          return Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                Text(
+                  score.get('match_name'),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    Text(
-                      score.get('match_name'),
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    Column(
                       children: [
-                        Column(
-                          children: [
-                            Text(
-                              score.get('score_team_a').toString(),
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                            Text(
-                              score.get('team_a'),
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                          ],
+                        Text(
+                          score.get('score_team_a').toString(),
+                          style: Theme.of(context).textTheme.titleLarge,
                         ),
-                        const Text('vs'),
-                        Column(
-                          children: [
-                            Text(
-                              score.get('score_team_b').toString(),
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                            Text(
-                              score.get('team_b'),
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                          ],
-                        )
+                        Text(
+                          score.get('team_a'),
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      ],
+                    ),
+                    const Text('vs'),
+                    Column(
+                      children: [
+                        Text(
+                          score.get('score_team_b').toString(),
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        Text(
+                          score.get('team_b'),
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
                       ],
                     )
                   ],
-                ),
-              );
-            } else {
-              return const SizedBox();
-            }
-          }
-          return const SizedBox();
+                )
+              ],
+            ),
+          );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('basketball')
+              .doc('2023_ban_vs_ind')
+              .update({'match_name': "Bangladesh vs India"});
+        },
+        child: const Text('Update'),
       ),
     );
   }
