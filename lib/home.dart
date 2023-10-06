54fa81be1bce3cdcfdb96d9d3791ce3eadea2979
MHFerdous <mahmudhossainferdous@gmail.com>
2023-10-06 23:37:34 +0600

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,119 +1,158 @@
-/*
 import 'package:flutter/material.dart';
-import 'package:mobile_application/second_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController _nameTEController = TextEditingController();
-  final TextEditingController _passwordTEController = TextEditingController();
-
-  GlobalKey<FormState> regForm = GlobalKey<FormState>();
-
-  bool _isObscure = true;
-
+  bool pressed = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Registration Form'),
+        title: const Text('Size Selector'),
+        centerTitle: true,
       ),
       body: Center(
-        child: Form(
-          key: regForm,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: _nameTEController,
-                  decoration: const InputDecoration(
-                    hintText: 'Name',
-                    label: Text(
-                      'Input your name',
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(),
-                  ),
-                  textInputAction: TextInputAction.next,
-                  validator: (String? value) {
-                    if (value?.trim().isEmpty ?? true) {
-                      return 'Please enter your name';
-                    }
-                    return null;
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      pressed = !pressed;
+                    });
                   },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: _passwordTEController,
-                  obscureText: _isObscure,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    label: const Text(
-                      'Input your password',
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                    border: const OutlineInputBorder(),
-                    focusedBorder: const OutlineInputBorder(),
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _isObscure = !_isObscure;
-                        });
-                      },
-                      icon: Icon(
-                          _isObscure ? Icons.visibility : Icons.visibility_off),
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    height: 40,
+                    width: 70,
+                    decoration: pressed
+                        ? const BoxDecoration(color: Colors.amber)
+                        : BoxDecoration(color: Colors.grey.shade500),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'S',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                  validator: (String? value) {
-                    if (value?.trim().isEmpty ?? true) {
-                      return 'Please enter your password';
-                    }
-                    if (value!.length < 6) {
-                      return 'Its too short';
-                    }
-                    return null;
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      pressed = !pressed;
+                    });
                   },
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (regForm.currentState!.validate()) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SecondScreen(),
-                          ),
-                        );
-                        _nameTEController.clear();
-                        _passwordTEController.clear();
-                      }
-                    },
-                    child: const Text('Sign in'),
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    height: 40,
+                    width: 70,
+                    decoration: pressed
+                        ? const BoxDecoration(color: Colors.amber)
+                        : BoxDecoration(color: Colors.grey.shade500),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'M',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
-              )
-            ],
-          ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      pressed = !pressed;
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    height: 40,
+                    width: 70,
+                    decoration: pressed
+                        ? const BoxDecoration(color: Colors.amber)
+                        : BoxDecoration(color: Colors.grey.shade500),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'L',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      pressed = !pressed;
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    height: 40,
+                    width: 70,
+                    decoration: pressed
+                        ? const BoxDecoration(color: Colors.amber)
+                        : BoxDecoration(color: Colors.grey.shade500),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'XL',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      pressed = !pressed;
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    height: 40,
+                    width: 70,
+                    decoration: pressed
+                        ? const BoxDecoration(color: Colors.amber)
+                        : BoxDecoration(color: Colors.grey.shade500),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'XXL',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      pressed = !pressed;
+                    });
+
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    height: 40,
+                    width: 70,
+                    decoration: pressed
+                        ? const BoxDecoration(color: Colors.amber)
+                        : BoxDecoration(color: Colors.grey.shade500),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'XXXL',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
   }
 }
-*/
--- lib/main.dart
+++ lib/main.dart
@@ -1,159 +1,19 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Product List App',
-      home: ProductList(),
-    );
-  }
-}
-
-class ProductList extends StatefulWidget {
-  @override
-  _ProductListState createState() => _ProductListState();
-}
-
-class _ProductListState extends State<ProductList> {
-  List<Product> products = [
-    Product(name: "iPhone 13", price: 799.99),
-    Product(name: "Samsung Ultra", price: 999.99),
-    Product(name: "Google Pixel 6", price: 799.99),
-    Product(name: "OnePlus 9 Pro", price: 899.99),
-    Product(name: "Xiaomi Mi 11", price: 699.99),
-    Product(name: "Sony Xperia 1 III", price: 1099.99),
-    Product(name: "LG Velvet", price: 599.99),
-    Product(name: "Huawei P40 Pro", price: 899.99),
-    Product(name: "Motorola Edge", price: 699.99),
-    Product(name: "Oppo Find X3 Pro", price: 799.99),
-    Product(name: "ASUS ROG Phone 5", price: 1099.99),
-    Product(name: "Nokia 8.3", price: 599.99),
-  ];
-
-  int cartItemCount = 0;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("Product List")),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => CartPage(cartItemCount: cartItemCount),
-            ),
-          );
-        },
-        child: Icon(Icons.shopping_cart),
-      ),
-      body: ListView.builder(
-        shrinkWrap: true,
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return SingleChildScrollView(
-            child: ListTile(
-              title: Text(
-                products[index].name,
-                style: TextStyle(fontSize: 14),
-              ),
-              subtitle: Text(
-                "\$${products[index].price.toStringAsFixed(2)}",
-                style: TextStyle(fontSize: 10),
-              ),
-              trailing: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Count: ${products[index].counter}',
-                    style: TextStyle(fontSize: 10),
-                  ),
-                  Container(
-                    height: 35,
-                    width: 60,
-                    child: TextButton(
-                      onPressed: () {
-                        setState(
-                          () {
-                            products[index].incrementCounter();
-                            if (products[index].counter == 5) {
-                              showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return AlertDialog(
-                                    title: Text("Congratulations!"),
-                                    content: Text(
-                                      "You've bought 5 ${products[index].name}!",
-                                    ),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text("OK"),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            }
-                            cartItemCount++;
-                          },
-                        );
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white,
-                      ),
-                      child: Text(
-                        "Buy Now",
-                        style: TextStyle(fontSize: 10),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class Product {
-  final String name;
-  final double price;
-  int counter = 0;
-
-  Product({required this.name, required this.price});
-
-  void incrementCounter() {
-    counter++;
-  }
-}
-
-class CartPage extends StatelessWidget {
-  final int cartItemCount;
-
-  CartPage({required this.cartItemCount});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Cart"),
-      ),
-      body: Center(
-        child: Text("Total Items in Cart: $cartItemCount"),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
